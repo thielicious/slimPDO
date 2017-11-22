81a96a971432a57c81434db07d7b1376b9650640
Thielicious <TheDefinitionist@users.noreply.github.com>
2017-11-22 17:26:16 +0100

    added interface

--- slimPDO.php
+++ slimPDO.php
@@ -1,54 +1,65 @@
 <?php
 
 	/*
-		slimPDO v1.0 
+		slimPDO v1.1 
 		(c) 2017 by Thielicious
 		
 		Just a small tweaked PDO extension for my own personal use.
 	*/
-
 	
+	
+	namespace Thielicious\utils\slimPDO;
+	
+	use PDO;
+	
+	interface slimStruct {
+
+		public function prep(string $sql, array $bind = null);
+		public function qry(string $sql);
+
+	}
+
+
 	if (!is_subclass_of("slimPDO","PDO")) {
-		
-		abstract class Binder extends PDO {
-			
-			public function prep($sql, array $bind = null) {
+
+		abstract class slimPDOInit extends PDO {
+
+			public function prep(string $sql, array $bind = null) {
 				$stmt = $this->prepare($sql);
 				$stmt->execute($bind);
 				return $stmt;
 			}
 			
-			abstract public function qry($sql);
-			
+			abstract public function qry(string $sql);
+
 		}
 		
-		
-		final class slimPDO extends Binder {
-			
-			public function qry($sql) {
+		final class slimPDO extends slimPDOInit implements slimStruct {
+
+			public function qry(string $sql) {
 				return parent::prep($sql);
 			}
-			
+
 		}
 		
 	}
-
-
+	
+	
 	trait DB {
 		
 		protected
-			$pdo;
-			
+			$pdo;		
+
 		protected function connect($host, $db, $user, $pass) {
 			try {
 				$this->pdo = new slimPDO(
 					"mysql:host=".$host.";dbname=".$db, $user, $pass,
-					array(
+					[
 						PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
 						PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 						PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
 						PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
-					)
+					]
 				);
 			} catch (PDOException $e) {
 				exit($e->getMessage());
